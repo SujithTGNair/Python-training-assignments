1ac10c0ba70d6acc7bdd895fd4213e0e78e794b3
SujithTGNair <57579157+SujithTGNair@users.noreply.github.com>
2019-11-20 11:30:44 +0530

    Add files via upload

---
+++ 20-11-2019 Prime number check.py
@@ -0,0 +1,17 @@
+c=int(input())
+if c<2:
+    print ("This is a composite number")
+elif c==2==2:
+    print("This is a prime number")
+elif c%2==0:
+    print("This is not a prime number")
+elif c%3==0:
+    print("This is not a prime number")
+elif c%5==0:
+    print("This is not a prime number")
+elif c%7==0:
+    print("This is not a prime number")
+elif c%11==0:
+    print("This is not a prime number")
+else:
+    print("This is a prime number")
---
+++ 20-11-2019 Prime number of a range.py
@@ -0,0 +1,21 @@
+n=int(input())
+o=int(input())
+list1=[]
+for i in range(n,o):
+    if i<2:
+        None
+    elif i==2==2:
+        None 
+    elif i%2==0:
+        None
+    elif i%3==0:
+        None
+    elif i%5==0:
+        None
+    elif i%7==0:
+        None
+    elif i%11==0:
+        None
+    else:
+        list1.append(i)
+print(list1)
